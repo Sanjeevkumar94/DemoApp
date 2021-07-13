7e0e7173f2056a11f8593c98f8401c0c3b01f57c
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-07-13 20:01:53 +0530

    init

---
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.demoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        lifecycle.addObserver(Observer())
+        Log.d("Main","Activity OnCreate")
+    }
+}
